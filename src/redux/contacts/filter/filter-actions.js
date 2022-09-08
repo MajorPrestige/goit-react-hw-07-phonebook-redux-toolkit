b8b34f85d51c66d091ce214ae6e1be42430fa394
Denis Dashkevich <majorprestige99@gmail.com>
2022-09-08 18:47:48 +0300

    Update filter-actions.js

--- src/redux/contacts/filter/filter-actions.js
+++ src/redux/contacts/filter/filter-actions.js
@@ -1,12 +1,3 @@
 import { createAction } from '@reduxjs/toolkit';
 
 export const setFilter = createAction('contacts/filter/set');
-
-// import { FILTER_CONTACTS } from 'redux/types';
-
-// export const filterContacts = payload => {
-//   return {
-//     type: FILTER_CONTACTS,
-//     payload,
-//   };
-// };
